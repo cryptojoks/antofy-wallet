da9a43116135841b9fae25bd0ef0eea7c1d557c6
Crypto Joks <98596722+cryptojoks@users.noreply.github.com>
2023-05-28 17:40:17 -0600

    Update GetWalletActions.js

--- src/components/GetWallet/GetWalletActions.js
+++ src/components/GetWallet/GetWalletActions.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Tooltip } from 'react-tooltip'
 import Icon from '../Icon'
 import Button from '../Button'
-import AndroidQrCode from './qrcode-get-wallet.svg'
+import AndroidQrCode from './qrcode-get-wallet.png'
 
 import './GetWalletActions.scss'
 

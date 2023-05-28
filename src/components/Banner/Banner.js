addc77b8fc278200f540b5948051cb3ef2b472f9
Crypto Joks <98596722+cryptojoks@users.noreply.github.com>
2023-05-28 16:35:12 -0600

    Update Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -15,14 +15,14 @@ function Banner() {
           <img
             className="Banner-img"
             src="/images/banner/wallet.png"
-            alt="Unstoppable wallet"
+            alt="Antofy wallet"
             srcSet="/images/banner/wallet@2x.png 2x"
           />
         </div>
         <div className="Banner-end">
           <div className="Banner-end-content">
             <div className="Banner-title text-gradient">
-              Be Unstoppable
+              Be Decentralized
             </div>
             <div className="Banner-title-slogan my-2 py-2 mb-3">
               Unchain Assets | Go Borderless | Stay Private

00655833433ca84e0d408e9fa40e1375e01b46b3
cryptojoks <98596722+cryptojoks@users.noreply.github.com>
2023-05-28 22:11:57 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Demo/Demo.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react'
+import cn from 'classnames'
+import Container from '../Container'
+import YoutubeEmbed from '../Video/YoutubeEmbed'
+import './Demo.scss'
+
+import { ReactComponent as BitcoinWallet } from './bitcoin-wallet.svg'
+import { ReactComponent as MultiWallet } from './multi-wallet.svg'
+import { ReactComponent as NftWallet } from './nft-wallet.svg'
+import { ReactComponent as ForInvestors } from './for-investors.svg'
+import { ReactComponent as WatchAddress } from './watch-address.svg'
+import { ReactComponent as AdvancedSearch } from './search.svg'
+import { ReactComponent as Analytics } from './analytics.svg'
+
+function Demo() {
+  const [active, setActive] = useState('multiWallet')
+  const isActive = name => active === name ? 'active' : null
+  const setState = val => () => setActive(val)
+
+  const map = {
+    bitcoinWallet: '9gT11AgG_s0',
+    multiWallet: '5Z2CzFVjksA',
+    nftWallet: 'ZjNFheebl2U',
+    watchAddress: 'ii4j-Iv7eyY',
+    forInvestors: 'xa2cvT6FagA',
+    advancedSearch: 'XIdIsKv16gU',
+    analytics: 'c7tDslRVFgA'
+  }
+
+  return (
+    <Container>
+      <div className="Demo Section-space-top Section-space-bottom">
+        <div className="Page-header-text text-center pb-5 text-gradient">
+          Demos
+        </div>
+
+        <div className="Demo-container mt-5 pt-5">
+          <div className="Demo-content">
+            <TabContent key={active} isActive={isActive}>
+              <YoutubeEmbed embedId={map[active]} />
+            </TabContent>
+          </div>
+          <div className="Demo-tabs">
+            <div className="tablink-header">Use Cases</div>
+            <Tab item="multiWallet" Icon={MultiWallet} name="Multi Wallet" isActive={isActive} setActive={setState} />
+            <Tab item="nftWallet" Icon={NftWallet} name="NFT Wallet" isActive={isActive} setActive={setState} />
+            <Tab item="bitcoinWallet" Icon={BitcoinWallet} name="Bitcoin Wallet" isActive={isActive} setActive={setState} />
+            <Tab item="watchAddress" Icon={WatchAddress} name="Monitoring Wallet" isActive={isActive} setActive={setState} />
+            <Tab item="forInvestors" Icon={ForInvestors} name="Investor Wallet" isActive={isActive} setActive={setState} />
+            <Tab item="advancedSearch" Icon={AdvancedSearch} name="Advanced Market Search" isActive={isActive} setActive={setState} />
+            <Tab item="analytics" Icon={Analytics} name="Advanced Token Analytics" isActive={isActive} setActive={setState} />
+          </div>
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+
+const Tab = ({ name, item, Icon, isDisabled, isActive = () => false, setActive = () => false }) => (
+  <div className={cn('tablink', isActive(item), { disabled: isDisabled })} onClick={setActive(item)}>
+    <Icon />
+    <span className="ms-3">{name}</span>
+  </div>
+)
+
+const TabContent = ({ children }) => (
+  <div className="tabcontent">
+    {children}
+  </div>
+)
+
+export default Demo
---
+++ src/components/GetWallet/GetWalletActions.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Tooltip } from 'react-tooltip'
+import Icon from '../Icon'
+import Button from '../Button'
+import AndroidQrCode from './qrcode-get-wallet.svg'
+
+import './GetWalletActions.scss'
+
+function GetWallet() {
+  return (
+    <div className="Wallet-actions">
+      <div className="Banner-actions">
+        <Tooltip
+          id="registerTip"
+          data-tooltip-place="top"
+          variant="light"
+          className="QqCodeTooltip"
+          content={<img src={AndroidQrCode} alt="" />}
+        />
+        <Button
+          className="Button-circle Button-nowrap"
+          text="Google Play"
+          icon="google-play"
+          link="https://play.google.com/store/apps/details?id=io.horizontalsystems.bankwallet" yellow newTab />
+        <Button
+          className="Button-circle Button-nowrap"
+          text="App Store"
+          icon="app-store"
+          link="https://itunes.apple.com/app/bank-bitcoin-wallet/id1447619907?ls=1&mt=8" yellow newTab />
+        <div className="sm-hidden" role="button" data-tooltip-id="registerTip">
+          <Icon name="qr-code" />
+        </div>
+      </div>
+      <div className="Banner-links">
+        <Button
+          className="Button-circle Button-download"
+          text="F-Droid"
+          icon="f-droid"
+          link="https://f-droid.org/en/packages/io.horizontalsystems.bankwallet/" newTab />
+        <Button
+          className="Button-circle Button-download"
+          text="APK file"
+          icon="android"
+          link="https://github.com/horizontalsystems/unstoppable-wallet-android/releases" newTab />
+        <Button
+          className="Button-circle Button-download"
+          text="Github"
+          icon="github"
+          link="https://github.com/horizontalsystems" newTab />
+        <Button
+          className="Button-circle Button-download"
+          text="Gitlab"
+          icon="gitlab"
+          link="https://gitlab.com/horizontal_systems" newTab />
+      </div>
+    </div>
+  )
+}
+
+export default GetWallet

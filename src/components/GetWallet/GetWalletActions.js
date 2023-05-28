b2005ce73744e3da0930dc0e10ce1ef48faf0196
Crypto Joks <98596722+cryptojoks@users.noreply.github.com>
2023-05-28 17:16:26 -0600

    Update GetWalletActions.js

--- src/components/GetWallet/GetWalletActions.js
+++ src/components/GetWallet/GetWalletActions.js
@@ -21,12 +21,12 @@ function GetWallet() {
           className="Button-circle Button-nowrap"
           text="Google Play"
           icon="google-play"
-          link="https://play.google.com/store/apps/details?id=io.horizontalsystems.bankwallet" yellow newTab />
+          link="https://play.google.com/store/apps/" yellow newTab />
         <Button
           className="Button-circle Button-nowrap"
           text="App Store"
           icon="app-store"
-          link="https://itunes.apple.com/app/bank-bitcoin-wallet/id1447619907?ls=1&mt=8" yellow newTab />
+          link="https://itunes.apple.com/app/" yellow newTab />
         <div className="sm-hidden" role="button" data-tooltip-id="registerTip">
           <Icon name="qr-code" />
         </div>
@@ -34,24 +34,19 @@ function GetWallet() {
       <div className="Banner-links">
         <Button
           className="Button-circle Button-download"
-          text="F-Droid"
-          icon="f-droid"
-          link="https://f-droid.org/en/packages/io.horizontalsystems.bankwallet/" newTab />
-        <Button
-          className="Button-circle Button-download"
           text="APK file"
           icon="android"
-          link="https://github.com/horizontalsystems/unstoppable-wallet-android/releases" newTab />
+          link="https://github.com/antofy/antofy-wallet-android/" newTab />
         <Button
           className="Button-circle Button-download"
           text="Github"
           icon="github"
-          link="https://github.com/horizontalsystems" newTab />
+          link="https://github.com/antofy" newTab />
         <Button
           className="Button-circle Button-download"
           text="Gitlab"
           icon="gitlab"
-          link="https://gitlab.com/horizontal_systems" newTab />
+          link="https://gitlab.com/antofy" newTab />
       </div>
     </div>
   )

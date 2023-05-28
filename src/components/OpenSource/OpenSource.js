5a081b0f13213c747e051a9afd79b32c8aebb325
Crypto Joks <98596722+cryptojoks@users.noreply.github.com>
2023-05-28 17:19:52 -0600

    Update OpenSource.js

--- src/components/OpenSource/OpenSource.js
+++ src/components/OpenSource/OpenSource.js
@@ -19,7 +19,7 @@ function OpenSource() {
             title="Approved By Bitcoin.org"
             info={
               <>
-                Unstoppable has been listed by <a href="https://bitcoin.org/en/wallets/mobile/ios/unstoppable/" target="_blank"
+                Antofy has been <a href="https://bitcoin.org/" target="_blank"
                                                   rel="noopener noreferrer" className="color-link">Bitcoin.org</a>, one of the leading informational
                 resources for Bitcoin,
                 and recommended for both new and experienced users alike.
@@ -30,8 +30,8 @@ function OpenSource() {
             title="Audited By Certik"
             info={
               <>
-                Unstoppable was audited by <a href="https://www.certik.org/projects/unstoppable.money" target="_blank" rel="noopener noreferrer"
-                                              className="color-link">Certik.org</a> where both iOS and Android versions of the app were checked for
+                Antofy will be audited by <a href="https://www.certik.org/" target="_blank" rel="noopener noreferrer"
+                                              className="color-link">Certik.org</a> both iOS and Android versions of the app will be checked for
                 potential backdoors and vulnerabilities. No major or minor issues were found.
               </>
             } />
@@ -40,8 +40,8 @@ function OpenSource() {
             title="Audited By Immunefi"
             info={
               <>
-                Unstoppable was audited by <a href="https://immunefi.com/bounty/unstoppablewallet/" target="_blank" rel="noopener noreferrer"
-                                              className="color-link">Immunefi.com</a> where both iOS and Android versions of the app were checked for
+                Antofy will be audited by <a href="https://immunefi.com/" target="_blank" rel="noopener noreferrer"
+                                              className="color-link">Immunefi.com</a> both iOS and Android versions of the app will be checked for
                 potential backdoors and vulnerabilities. No major or minor issues were found.
               </>
             } />
@@ -50,9 +50,9 @@ function OpenSource() {
             title="Approved By Wallet Scrutiny"
             info={
               <>
-                The <a href="https://walletscrutiny.com/android/io.horizontalsystems.bankwallet/" target="_blank" rel="noopener noreferrer"
+                The <a href="https://walletscrutiny.com/" target="_blank" rel="noopener noreferrer"
                        className="color-link">WalletScrutiny.com</a> verifies that cryptocurrency wallets are indeed using the same publicly open
-                code they claim to. Unstoppable is verifiably open source wallet.
+                code they claim to. Antofy is verifiably open source wallet.
               </>
             } />
         </Slider>
